6122a034b714ef7c5ca12b254da1ddf5912a236a
Ankan Kushwaha <ankankushwaha30@gmail.com>
2025-03-07 23:58:34 +0530

    commit

---
+++ Pattern_Double Hollow Right Triangles.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+int main() {
+
+    int n;
+    scanf("%d",&n);
+    if(n>1)
+    {
+        for(int i=1;i<=n;++i)
+        {
+            for(int j=1;j<=i;++j)
+            {
+                if(j==i || j==1 || i==n)
+                {
+                    printf("*");
+                }
+                else{
+                    printf(" ");
+                }
+            }
+            for(int k=1;k<=2*(n-i);++k)
+            {
+                printf(" ");
+            }
+            for(int j=1;j<=i;++j)
+            {
+                if(j==i || j==1 || i==n)
+                {
+                    printf("*");
+                }
+                else{
+                    printf(" ");
+                }
+            }
+            
+            printf("\n");
+        }
+    }
+    else{
+        printf("Shape Not Possible");
+    }
+    return 0;
+}
