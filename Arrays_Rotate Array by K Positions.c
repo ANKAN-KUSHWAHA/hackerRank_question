1e024317d677ef18f876fe44463649fefe3a864b
Ankan Kushwaha <ankankushwaha30@gmail.com>
2025-04-28 02:21:23 +0530

    commit

---
+++ Arrays_Rotate Array by K Positions.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+
+void rotateArray(int arr[], int n, int k) {
+    
+    
+         k=k%n;
+     
+    int temp[k];
+    for(int i=0;i<k;++i){
+        temp[i]=arr[i];
+    }
+    for(int i=0;i<n-k;++i){
+        arr[i]=arr[i+k];
+    }
+    for(int i=0;i<k;++i){
+        arr[n-k+i]=temp[i];
+    } 
+}
+
+
+
+int main() {
+    int n, k;
+    
+    scanf("%d", &n);
+    int arr[n];
+    for (int i = 0; i < n; ++i) {
+        scanf("%d", &arr[i]);
+    }
+    scanf("%d", &k);
+
+    
+    rotateArray(arr, n, k);
+    
+    
+    for (int i = 0; i < n; ++i) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
